6c4da21f33dbafcf35be7ed78bf460898ee22553
David Banks <david.banks0889@gmail.com>
2015-11-16 20:30:37 -0600

    Merge origin/origin
    Conflicts:
    	nbproject/project.properties
    	public_html/index.html

--- src/cit360/sandbox/CIT360SandBox.java
+++ src/cit360/sandbox/CIT360SandBox.java
@@ -46,96 +46,22 @@ import javax.net.ssl.HttpsURLConnection;
  * @author DBanks
  */
 public class CIT360SandBox {
-    
-private final String USER_AGENT = "Mozilla/5.0";
+    public static String firstName; 
+    static Iterable<String[]> Selections; 
+    public final static Sql_Connect Sql_Connect = new Sql_Connect(); 
+    public final static Menu App = new Menu(); 
 
 	public static void main(String[] args) throws Exception {
+                
+            Scanner input = new Scanner (System.in); 
+            System.out.println("What is your name?");
+            CIT360SandBox.firstName = input.next(); 
+            
+            System.out.println("Welcome " + firstName + "!");
+            
+            CIT360SandBox.App.mainMenu();
 
-		CIT360SandBox http = new CIT360SandBox();
-
-		System.out.println("Testing 1 - Send Http GET request");
-		http.sendGet();
-		
-		System.out.println("\nTesting 2 - Send Http POST request");
-		http.sendPost();
-
-	}
-
-	// HTTP GET request
-	private void sendGet() throws Exception {
-
-		String url = "http://www.google.com";
-		
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		// optional default is GET
-		con.setRequestMethod("GET");
-
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		System.out.println(response.toString());
-
-	}
-	
-	// HTTP POST request
-	private void sendPost() throws Exception {
-
-		String url = "http://apple.com";
-		URL obj = new URL(url);
-		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
-		//add reuqest header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
-		
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-		
-		//print result
-		System.out.println(response.toString());
-
-	}
-  
-
+        }
 }
         
             /*Scanner input = new Scanner(System.in);   // gets input using scanner and puts it into variable firstName
