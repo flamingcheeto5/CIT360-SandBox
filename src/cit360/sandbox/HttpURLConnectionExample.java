1c6e29b0031b2f13595d6b3f9aa6f3a183209a3b
David Banks <david.banks0889@gmail.com>
2015-11-16 20:20:58 -0600

    Merge origin/origin
    Conflicts:
    	nbproject/project.properties
    	public_html/index.html

--- src/cit360/sandbox/HttpURLConnectionExample.java
+++ src/cit360/sandbox/HttpURLConnectionExample.java
@@ -5,31 +5,7 @@
  */
 package cit360.sandbox;
 
-import static java.lang.Compiler.command;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Iterator;
-import org.hibernate.Query;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.Date;
-import java.util.HashMap;
-import org.json.JSONException;
-import java.net.URL; 
-import java.io.BufferedReader;
-import java.io.IOException; 
-import java.io.InputStream; 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection; 
-import java.net.URL; 
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -38,20 +14,18 @@ import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
-
-
-
 /**
  *
- * @author DBanks
+ * @author dbanks
  */
-public class CIT360SandBox {
-    
-private final String USER_AGENT = "Mozilla/5.0";
+ 
+public class HttpURLConnectionExample {
 
-	public static void main(String[] args) throws Exception {
+	private final String USER_AGENT = "Mozilla/5.0";
 
-		CIT360SandBox http = new CIT360SandBox();
+	public static void connection(String[] args) throws Exception {
+
+		HttpURLConnectionExample http = new HttpURLConnectionExample();
 
 		System.out.println("Testing 1 - Send Http GET request");
 		http.sendGet();
@@ -97,7 +71,7 @@ private final String USER_AGENT = "Mozilla/5.0";
 	// HTTP POST request
 	private void sendPost() throws Exception {
 
-		String url = "http://apple.com";
+		String url = "https://selfsolve.apple.com/wcResults.do";
 		URL obj = new URL(url);
 		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
@@ -134,34 +108,5 @@ private final String USER_AGENT = "Mozilla/5.0";
 		System.out.println(response.toString());
 
 	}
-  
 
 }
-        
-            /*Scanner input = new Scanner(System.in);   // gets input using scanner and puts it into variable firstName
-            System.out.println("What is your name?");
-            CIT360SandBox.firstName = input.next();
-            
-            System.out.println("Welcome " + firstName + "!");
-            
-            CIT360SandBox.App.mainMenu();
-    }
-   }    
-    /**public static void main(String[] args) {     
-      List moses = new ArrayList();  // A List contains instances of Object. Upcast ArrayList to List
-      moses.add("Genesis");            // add() takes Object. String upcast to Object implicitly
-      moses.add("Exodus");
-      moses.add("Leviticus");
-      moses.add("Numbers");
-      moses.add("Deuteronomy");
-      System.out.println(moses);     // prints the list of the first 5 books in the Old Testament
- 
-      // Get a "iterator" instance from List to iterate thru all the elements of the List
-      Iterator iter = moses.iterator();
-      while (iter.hasNext()) {      // any more element
-         // Retrieve the next element, explicitly downcast from Object back to String
-         String str = (String)iter.next();
-         System.out.println(str);
-        }
-    }
-  */
